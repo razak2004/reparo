9bcb183dbba9db2b3c12dd3acb3cc6a408ac3194
razak2004 <abdulrazak.nasriudeen@fssa.freshworks.com>
2023-08-07 16:02:31 +0530

    login page created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="login">
+      <div className="circle"></div>
+      <div className="loginContainer">
+        <h2>Login</h2>
+        <InputContainer />
+        <InputContainer />
+      </div>
+      <div className="circ2"></div>
+    </div>
+  );
+}
+
+function InputContainer() {
+  return (
+    <div className="inputContainer">
+      <p>Mobile Number </p>
+      <input />
     </div>
   );
 }
