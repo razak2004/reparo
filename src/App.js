20f467e278217cb698d5b5a5da0bbad7dda93249
razak2004 <abdulrazak.nasriudeen@fssa.freshworks.com>
2023-08-07 16:55:25 +0530

    login page front end completed

--- src/App.js
+++ src/App.js
@@ -4,21 +4,26 @@ function App() {
   return (
     <div className="login">
       <div className="circle"></div>
-      <div className="loginContainer">
+      <form className="loginContainer">
         <h2>Login</h2>
-        <InputContainer />
-        <InputContainer />
-      </div>
+        <InputContainer name="Mobile Number" id="number" span="call" />
+        <InputContainer name="Password" id="password" span="dialpad" />
+        <h5>forget password ?</h5>
+        <button type="submit">Submit</button>
+      </form>
       <div className="circ2"></div>
     </div>
   );
 }
 
-function InputContainer() {
+function InputContainer(prop) {
   return (
     <div className="inputContainer">
-      <p>Mobile Number </p>
-      <input />
+      <p>{prop.name}</p>
+      <div className="cont">
+        <span className="material-symbols-outlined">{prop.span}</span>
+        <input id={prop.id} />
+      </div>
     </div>
   );
 }
